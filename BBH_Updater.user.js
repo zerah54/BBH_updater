4217c78b6ba68160f384f26f99ce4dfc5b7bea67
Zerah <7262531+zerah54@users.noreply.github.com>
2021-05-24 00:30:08 +0200

    Update BBH_Updater.user.js

--- BBH_Updater.user.js
+++ BBH_Updater.user.js
@@ -2,13 +2,13 @@
 // @name         BBH Updater
 // @description  Update BBH from the world beyond
 // @author       Zerah
-// @version      0.1a
+// @version      0.1
 // @match        https://zombvival.de/myhordes/*
 // @match        https://bbh2.fred26.fr/*
-// @require http://userscripts-mirror.org/scripts/source/107941.user.js
-// @grant GM_setValue
-// @grant GM_getValue
-// @grant GM_addValueChangeListener
+// @require      http://userscripts-mirror.org/scripts/source/107941.user.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addValueChangeListener
 // ==/UserScript==
 
 (function() {
